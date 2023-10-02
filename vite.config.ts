6abe7b5c2c98da16aa819f4f4e80c7afc941d86f
ateals <ateals@icloud.com>
2023-10-02 14:42:05 +0900

    init: dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+import react from "@vitejs/plugin-react-swc";
+import tsConfigPaths from "vite-tsconfig-paths";
+
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react(), tsConfigPaths()],
+});
