895e071bd04d774845a46e3f2fdce9ab92480f31
ateals <ateals@icloud.com>
2023-10-03 15:17:14 +0900

    feat: global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header";
+import { createGlobalStyle } from "styled-components";
+
+import "@/styles/global.css";
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        background-color : ${(props) => props.theme.default.black};
+        color: ${(props) => props.theme.default.white};
+        width : 1000px;
+        margin : auto;
+    }
+`;
 
 const App = () => {
     return (
         <>
+            <GlobalStyle />
             <Header />
             <Outlet />
         </>
