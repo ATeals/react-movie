521ac314d27008d402b3b8f4c837fd5da0c4a5a0
ateals <ateals@icloud.com>
2024-01-15 20:26:50 +0900

    feat: vite-plugin-federation

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,25 @@ import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react-swc";
 import tsConfigPaths from "vite-tsconfig-paths";
+import federation from "@originjs/vite-plugin-federation";
 
 export default defineConfig({
   base: "/react-movie/",
-  plugins: [react(), tsConfigPaths()],
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    federation({
+      name: "remote-app",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./ReactMovie": "./src/App",
+      },
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
 });
