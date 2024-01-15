f0d455e9715363822b735748eccc40da09f5a139
ateals <ateals@icloud.com>
2024-01-15 20:14:09 +0900

    feat: depoly ci/cd

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    plugins: [react(), tsConfigPaths()],
+  base: "/react-movie/",
+  plugins: [react(), tsConfigPaths()],
 });
