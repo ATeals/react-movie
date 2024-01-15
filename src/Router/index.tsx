b45daab213a0535815a2885272383e3acd5e19c5
ateals <ateals@icloud.com>
2024-01-15 20:16:44 +0900

    fix: router basename

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -5,21 +5,26 @@ import App from "@/App";
 import { MovieDetail, Movies } from "@/pages";
 import { PATH } from "@/constants";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: PATH.ROOT,
+      element: <App />,
+      children: [
+        {
+          path: PATH.DYNAMIC_MOVIS,
+          element: <Movies />,
+          children: [
+            {
+              path: PATH.DETAIL + "/:id",
+              element: <MovieDetail />,
+            },
+          ],
+        },
+      ],
+    },
+  ],
   {
-    path: PATH.ROOT,
-    element: <App />,
-    children: [
-      {
-        path: PATH.DYNAMIC_MOVIS,
-        element: <Movies />,
-        children: [
-          {
-            path: PATH.DETAIL + "/:id",
-            element: <MovieDetail />,
-          },
-        ],
-      },
-    ],
-  },
-]);
+    basename: "/react-movie/",
+  }
+);
