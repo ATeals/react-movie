c517b9e072078f88728d412faecc6552d1da3ab8
ateals <ateals@icloud.com>
2024-01-16 18:58:38 +0900

    fix: remote route component

--- src/remote.tsx
+++ src/remote.tsx
@@ -1,12 +1,22 @@
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { QueryProvider, RouterProvider, ThemeProvier } from "./components/Providers";
+import { QueryProvider, ThemeProvier } from "./components/Providers";
+import { Route, Routes } from "react-router-dom";
+import { PATH } from "./constants";
+import App from "./App";
+import { MovieDetail, Movies } from "./pages";
 
 const Remote = () => {
   return (
     <QueryProvider>
       <ThemeProvier>
         <ReactQueryDevtools initialIsOpen={true} />
-        <RouterProvider />
+        <Routes>
+          <Route path={`${PATH.ROOT}/*`} element={<App />}>
+            <Route path={`${PATH.DYNAMIC_MOVIS}/*`} element={<Movies />}>
+              <Route path={`${PATH.DETAIL}/:id`} element={<MovieDetail />} />
+            </Route>
+          </Route>
+        </Routes>
       </ThemeProvier>
     </QueryProvider>
   );
